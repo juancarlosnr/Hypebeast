9a307df08afcd1d8e117b74e119879ccb538e796
juancarlosnr <juancarlosnr.17@gmail.com>
2021-03-13 09:49:56 +0100

    Bottom Navigation Bar

--- app/src/main/java/com/example/hypebeast/MainActivity.kt
+++ app/src/main/java/com/example/hypebeast/MainActivity.kt
@@ -2,15 +2,37 @@ package com.example.hypebeast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import com.example.hypebeast.databinding.ActivityMainBinding
+import com.example.hypebeast.ui.home.HomeFragment
+import com.example.hypebeast.ui.profile.ProfileFragment
+import com.example.hypebeast.ui.sneakers.SneakersFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val homeFragment = HomeFragment()
+        val sneakersFragment = SneakersFragment()
+        val profileFragment = ProfileFragment()
+
+        makeCurrentFragment(homeFragment)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_calendar -> makeCurrentFragment(sneakersFragment)
+                R.id.ic_profile -> makeCurrentFragment(profileFragment)
+            }
+            true
+        }
+
+
     }
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }

f82525bd256f01f37404e4ec8c4cb2fd90bce840
juancarlosnr <juancarlosnr.17@gmail.com>
2021-03-15 16:36:15 +0100

    Bottom App Bar Completado

--- app/src/main/java/com/example/hypebeast/MainActivity.kt
+++ app/src/main/java/com/example/hypebeast/MainActivity.kt
@@ -3,36 +3,27 @@ package com.example.hypebeast
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.hypebeast.databinding.ActivityMainBinding
 import com.example.hypebeast.ui.home.HomeFragment
 import com.example.hypebeast.ui.profile.ProfileFragment
 import com.example.hypebeast.ui.sneakers.SneakersFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val homeFragment = HomeFragment()
-        val sneakersFragment = SneakersFragment()
-        val profileFragment = ProfileFragment()
 
-        makeCurrentFragment(homeFragment)
-        binding.bottomNavigation.setOnNavigationItemSelectedListener {
-            when(it.itemId){
-                R.id.ic_home -> makeCurrentFragment(homeFragment)
-                R.id.ic_calendar -> makeCurrentFragment(sneakersFragment)
-                R.id.ic_profile -> makeCurrentFragment(profileFragment)
-            }
-            true
-        }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(R.id.fragments)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.sneakersFragment, R.id.profileFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
-
+        bottomNavigationView.setupWithNavController(navController)
     }
-    private fun makeCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fl_wrapper, fragment)
-            commit()
-        }
+
 }
